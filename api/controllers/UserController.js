5e1e59ab367251af125f7a3d7825369665abf460
JerryC <huangjerryc@gmail.com>
2015-06-02 21:44:49 +0800

    last push

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -10,8 +10,8 @@ module.exports = _.merge(
 
     // TODO should close '2254858394'
     getCurrentUser: function (req, res) {
-      var user = req.session.passport.user || '2254858394';
-//      var user = req.session.passport.user;
+//      var user = req.session.passport.user || '2254858394';
+      var user = req.session.passport.user;
       if (!user){
         return res.json('null');
       }
